7deffbd765fe54d784989b70012af5793a3f926f
Mary <kass.o@yandex.ru>
2024-02-15 14:59:33 +0300

    add

---
+++ catalog/views.py
@@ -0,0 +1,15 @@
+from django.shortcuts import render
+
+
+def index(request):
+
+    return render(request, 'main/home.html')
+
+
+def contacts(request):
+    if request.method == 'POST':
+        name = request.POST.get('name')
+        phone = request.POST.get('phone')
+        message = request.POST.get('message')
+        print(f'Name: {name}, Phone: {phone}, Message: {message}')
+    return render(request, 'main/contacts.html')
