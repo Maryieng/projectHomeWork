239f33316059b221a5d4457574b80b6cbfab14c3
Mary <kass.o@yandex.ru>
2024-02-22 19:57:32 +0300

    Changes have been made to the product module. new field added

--- catalog/models.py
+++ catalog/models.py
@@ -25,7 +25,7 @@ class Product(models.Model):
     price = models.IntegerField(verbose_name='Цена за покупку')
     created_at = models.DateField(auto_now_add=True, verbose_name='Дата создания')
     updated_at = models.DateField(auto_now=True, verbose_name='Дата последнего изменения')
-    manufactured_at = pass
+    manufactured_at = models.DateField( verbose_name='Дата производства продукта')
 
     def __str__(self):
         return f'{self.product_name} {self.description} {self.category}'
