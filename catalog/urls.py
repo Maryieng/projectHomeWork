0da093b58bcd18a79a39209c317cee6638d07e61
Mary <kass.o@yandex.ru>
2024-03-03 10:07:05 +0300

    product cards

--- catalog/urls.py
+++ catalog/urls.py
@@ -2,8 +2,9 @@ from django.urls import path
 
 from catalog.views import index, contacts, product_info
 
+app_name = 'shop'
 urlpatterns = [
     path('', index),
     path('contacts/', contacts),
-    path('products_info/<int:pk>/', product_info, name='products_info'),
+    path('<int:pk>/', product_info, name='product_info'),
 ]
