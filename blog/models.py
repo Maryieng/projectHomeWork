1ccc78941dad66c212ca187c0e747a9e9535bb83
Mary <kass.o@yandex.ru>
2024-03-07 19:18:20 +0300

    new blog post model

---
+++ blog/models.py
@@ -0,0 +1,20 @@
+from django.db import models
+
+NULLABLE = {'blank': True, 'null': True}
+
+
+class Blogpost(models.Model):
+    title = models.CharField(max_length=150, verbose_name='заголовок')
+    slug = models.CharField(max_length=150, verbose_name='slug', **NULLABLE)
+    content = models.TextField(verbose_name='содержимое')
+    preview = models.ImageField(upload_to='blogpost/', verbose_name='изображение', **NULLABLE)
+    created_at = models.DateField(auto_now_add=True, verbose_name='Дата создания')
+    publication_sign = models.BooleanField(default=True, verbose_name='Опубликован')
+    view_count = models.IntegerField(default=0, verbose_name='Количество просмотров')
+
+    def __str__(self):
+        return f'{self.title}'
+
+    class Meta:
+        verbose_name = ("Запись")
+        verbose_name_plural = ("Записи")
---
+++ blog/urls.py
@@ -0,0 +1,12 @@
+from django.urls import path
+
+app_name = 'blog'
+
+
+urlpatterns = [
+    # path('create/', MaterialCreateView.as_view(), name='create'),
+    # path('material_list/', MaterialListView.as_view(), name='list'),
+    # path('view/<int:pk>/', MaterialDetailView.as_view(), name='view'),
+    # path('edit/<int:pk>/', MaterialUpdateView.as_view(), name='edit'),
+    # path('delete/<int:pk>/', MaterialDeleteView.as_view(), name='delete'),
+]
