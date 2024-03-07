2cf02aa476ca8ac5e6df95b361077d3522232340
Mary <kass.o@yandex.ru>
2024-03-07 19:31:23 +0300

    migration has been applied, some of the URLs and controllers have been filled in

--- blog/urls.py
+++ blog/urls.py
@@ -1,11 +1,13 @@
 from django.urls import path
 
+from blog.views import BlogpostCreateView, BlogpostListView
+
 app_name = 'blog'
 
 
 urlpatterns = [
-    # path('create/', MaterialCreateView.as_view(), name='create'),
-    # path('material_list/', MaterialListView.as_view(), name='list'),
+     path('create/', BlogpostCreateView.as_view(), name='create'),
+     path('blog_list/', BlogpostListView.as_view(), name='blog_list'),
     # path('view/<int:pk>/', MaterialDetailView.as_view(), name='view'),
     # path('edit/<int:pk>/', MaterialUpdateView.as_view(), name='edit'),
     # path('delete/<int:pk>/', MaterialDeleteView.as_view(), name='delete'),
--- blog/views.py
+++ blog/views.py
@@ -1,3 +1,14 @@
-from django.shortcuts import render
+from django.urls import reverse_lazy
+from django.views.generic import CreateView, ListView
 
-# Create your views here.
+from blog.models import Blogpost
+
+
+class BlogpostCreateView(CreateView):
+    model = Blogpost
+    fields = ('title', 'content', 'created_at', 'view_count')
+    success_url = reverse_lazy('blog:blog_list')
+
+
+class BlogpostListView(ListView):
+    model = Blogpost
