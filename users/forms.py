11d5c99857202922f148ae18512dbfc6a97deacc
Mary <kass.o@yandex.ru>
2024-03-28 10:13:20 +0300

    password recovery

--- users/forms.py
+++ users/forms.py
@@ -1,5 +1,6 @@
-from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.forms import UserCreationForm, PasswordResetForm
 
+from catalog.forms import StyleFormMixin
 from users.models import User
 
 
@@ -8,3 +9,9 @@ class UserRegisterForm(UserCreationForm):
     class Meta:
         model = User
         fields = ('email', 'password1', 'password2')
+
+
+class UserPasswordRecoveryForm(StyleFormMixin, PasswordResetForm):
+    class Meta:
+        model = User
+        fields = ('email',)
--- users/views.py
+++ users/views.py
@@ -1,10 +1,21 @@
+import random
+
+from django.contrib.auth.hashers import make_password
 from django.core.mail import send_mail
 from django.urls import reverse_lazy
-from django.views.generic import CreateView
+from django.views.generic import CreateView, FormView
 
 from config import settings
-from users.forms import UserRegisterForm
+from users.forms import UserRegisterForm, UserPasswordRecoveryForm
 from users.models import User
+import string
+
+
+def generate_random_password():
+    length = 10
+    characters = string.ascii_letters + string.digits + string.punctuation
+    password = ''.join(random.choice(characters) for i in range(length))
+    return password
 
 
 class RegisterView(CreateView):
@@ -21,6 +32,26 @@ class RegisterView(CreateView):
             'Добро Пожаловать!',
             settings.EMAIL_HOST_USER,
             [email],
+            fail_silently=False,
+        )
+        return super().form_valid(form)
+
+class UserPasswordRecoveryView(FormView):
+    template_name = 'users/user_recovery_password.html'
+    form_class = UserPasswordRecoveryForm
+    success_url = reverse_lazy('users:login')
+
+    def form_valid(self, form):
+        email = form.cleaned_data.get('email')
+        user = User.objects.filter(email=email).first()
+        random_password = generate_random_password()
+        user.password = make_password(random_password)
+        user.save()
+        send_mail(
+            'Запрос на восстановление пароля',
+            f'Ваш новый пароль: {random_password}',
+            settings.EMAIL_HOST_USER,
+            [user.email],
             fail_silently=False,
         )
         return super().form_valid(form)
